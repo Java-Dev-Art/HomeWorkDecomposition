4ff8345e8dbcf15e05055ad38aa901bedad3cd57
Artur Markouski <veles.mk.12@gmail.com>
2023-01-25 18:28:04 +0100

    fix:add some parameters

--- src/test/java/TestTaskGreatestCommonDevisorOfTwoValues.java
+++ src/test/java/TestTaskGreatestCommonDevisorOfTwoValues.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(value = Parameterized.class)
-public class TestTaskDecompasion {
+public class TestTaskGreatestCommonDevisorOfTwoValues {
     private final int expected;
     private final int firstValue;
     private final int secondValue;
@@ -19,11 +19,16 @@ public class TestTaskDecompasion {
         return Arrays.asList(new Object[][]{
                 {4,28,16},
                 {6,24,18},
-                {3,12,9}
+                {3,12,9},
+                {4,12,16},
+                {8,40,32},
+                {2,54,86},
+                {7,98,35},
+                {2,112,82}
         });
     }
 
-    public TestTaskDecompasion(int expected, int firstValue, int secondValue) {
+    public TestTaskGreatestCommonDevisorOfTwoValues(int expected, int firstValue, int secondValue) {
         this.expected = expected;
         this.firstValue = firstValue;
         this.secondValue = secondValue;
